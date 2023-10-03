5ae02554392c43e910165cebc328e3532f12cbdd
ShubhamSatyabola <satyabolashubham@gmail.com>
2023-10-03 16:09:45 +0530

    importing css and using props

--- src/index.js
+++ src/index.js
@@ -1,45 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// raect fragment , jsx 
-// function Greeting(){
-//     return (
-//       <React.Fragment>
-//         <div>
-//           <h2>My First Component</h2>
-//           <ul>
-//             <li>
-//               <a href="#">hello world</a>
-//             </li>
-//           </ul>
-//         </div>
-//         <h1>only one return is possible</h1>
-//       </React.Fragment>
-//     );
-// }
 
-// <-------->react nesting of components<-------->
-function Greeting() {
+import './index.css'
+ //first project
+const author = "Chris Ferrie";
+const title = "8 Little Planets";
+const img = "https://images-na.ssl-images-amazon.com/images/I/91JFjdT4WCL._AC_UL600_SR600,400_.jpg";
+const BookList = () => {
   return (
-    <div>
-        <Person />
-        <Message/>
-    </div>
-      );
-}
-// two componenets person and message
-const Person = () => <h2>john doe</h2> 
-const Message = () => {
-    return <p>this is my message</p>
+    <section className="booklist">
+      <Book author={author} title={title} img={img} />
+      <Book author={author} title={title} img={img} />
+    </section>
+  );
 }
 
-// function Greeting() {
-//   return React.createElement(
-//     "div",
-//     {},
-//     React.createElement("h2", {}, "hello world")
-//   );
+//approach1 which includes nesting of multiple component
+
+// const Book = () => {
+//   return <article className='book'>
+//     <Image/>
+//     <Title/>
+//     <Author/>
+//   </article>
 // }
+
+// const Image = () => (
+//   <img
+//     src="https://images-na.ssl-images-amazon.com/images/I/91JFjdT4WCL._AC_UL600_SR600,400_.jpg"
+//     alt="8 Little Planets"
+//   />
+// );
+// const Title = () => <h2>8 Little Planets</h2>;
+
+// const Author = () => {
+//   return <h4>Chris Ferrie</h4>;
+// }
+
+// approach 2
+// const Book = () => {
+//   return (
+//     <article className="book">
+//       <img
+//         src="https://images-na.ssl-images-amazon.com/images/I/91JFjdT4WCL._AC_UL600_SR600,400_.jpg"
+//         alt="8 Little Planets"
+//       />
+//       <h2>8 Little Planets</h2>
+//       <h4>Chris Ferrie</h4>
+//     </article>
+//   );
+// };
+
+//----> Using props to make things dynamic
+
+const Book = (props) => {
+  return (
+    <article className="book">
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
+    </article>
+  );
+};
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Greeting/>)
+root.render(<BookList/>)
