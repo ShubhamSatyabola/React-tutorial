1b1698ec312585ca84b3ad1a92971a0f3310223c
ShubhamSatyabola <satyabolashubham@gmail.com>
2023-10-04 09:20:21 +0530

    props and childrean prop

--- src/index.js
+++ src/index.js
@@ -3,62 +3,54 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css'
  //first project
-const author = "Chris Ferrie";
-const title = "8 Little Planets";
-const img = "https://images-na.ssl-images-amazon.com/images/I/91JFjdT4WCL._AC_UL600_SR600,400_.jpg";
+const firstBook = {
+  author : "Chris Ferrie",
+  title : "8 Little Planets",
+  img : "https://images-na.ssl-images-amazon.com/images/I/91JFjdT4WCL._AC_UL600_SR600,400_.jpg"
+}
+const secondBook = {
+  author: "Rebecca Yarros",
+  title: "Iron Flame (The Empyrean, 2) ",
+  img: "https://images-na.ssl-images-amazon.com/images/I/91ke43dIxkL._AC_UL600_SR600,400_.jpg",
+};
+// const author = "Chris Ferrie";
+// const title = "8 Little Planets";
+// const img = "https://images-na.ssl-images-amazon.com/images/I/91JFjdT4WCL._AC_UL600_SR600,400_.jpg";
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book author={author} title={title} img={img} />
-      <Book author={author} title={title} img={img} />
+      <Book author={firstBook.author} title={firstBook.title} img={firstBook.img} >
+        <p>Lorem ipsum dolor sit amet consectetur.</p>
+        <button>click me</button>
+      </Book>
+      <Book author={secondBook.author} title={secondBook.title} img={secondBook.img} />
     </section>
   );
 }
 
-//approach1 which includes nesting of multiple component
+//----> Using props to make things dynamic
 
-// const Book = () => {
-//   return <article className='book'>
-//     <Image/>
-//     <Title/>
-//     <Author/>
-//   </article>
-// }
+//--->approach 1
 
-// const Image = () => (
-//   <img
-//     src="https://images-na.ssl-images-amazon.com/images/I/91JFjdT4WCL._AC_UL600_SR600,400_.jpg"
-//     alt="8 Little Planets"
-//   />
-// );
-// const Title = () => <h2>8 Little Planets</h2>;
-
-// const Author = () => {
-//   return <h4>Chris Ferrie</h4>;
-// }
-
-// approach 2
-// const Book = () => {
+// const Book = (props) => {
 //   return (
 //     <article className="book">
-//       <img
-//         src="https://images-na.ssl-images-amazon.com/images/I/91JFjdT4WCL._AC_UL600_SR600,400_.jpg"
-//         alt="8 Little Planets"
-//       />
-//       <h2>8 Little Planets</h2>
-//       <h4>Chris Ferrie</h4>
+//       <img src={props.img} alt={props.title} />
+//       <h2>{props.title}</h2>
+//       <h4>{props.author}</h4>
 //     </article>
 //   );
 // };
 
-//----> Using props to make things dynamic
-
-const Book = (props) => {
+// --> approach 2 to use props by destructuring the prop object
+const Book = ({ img, author, title, children}) => {
+  // const { img, author, title } = props;
   return (
     <article className="book">
-      <img src={props.img} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author}</h4>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      {children}
     </article>
   );
 };
