3f594e5027b47cbf2fb13f06622aae4bb9d9e163
ShubhamSatyabola <satyabolashubham@gmail.com>
2023-10-05 16:50:28 +0530

    es6 import and export

---
+++ src/books.js
@@ -0,0 +1,17 @@
+import img1 from './images/book1.jpg'
+import img2 from "./images/book2.jpg";
+
+export const books = [
+  {
+    author: "Chris Ferrie",
+    title: "8 Little Planets",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "Rebecca Yarros",
+    title: "Iron Flame (The Empyrean, 2) ",
+    img: img2,
+    id: 2,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -2,38 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css'
+
+import {books} from './books.js'
+
+import Book from './Book'
  //first project
 
-const books = [
-  {
-    author: "Chris Ferrie",
-    title: "8 Little Planets",
-    img: "https://images-na.ssl-images-amazon.com/images/I/91JFjdT4WCL._AC_UL600_SR600,400_.jpg",
-    id:1
-  },
-  {
-  author: "Rebecca Yarros",
-  title: "Iron Flame (The Empyrean, 2) ",
-  img: "https://images-na.ssl-images-amazon.com/images/I/91ke43dIxkL._AC_UL600_SR600,400_.jpg",
-  id:2
-}
-];
+
 
 
 
 const BookList = () => {
-  const getBook = (id) => {
-    const book = books.find(book=>book.id===id)
-    console.log(book)
-  }
-  
+
   
   return (
     <section className="booklist">
       {/* <EventExamples/> */}
-      {books.map((book) => {
+      {books.map((book,index) => {
         // const {img, title, author, id} = book
-        return <Book {...book} key={book.id} getBook={getBook} />;
+        return <Book {...book} key={book.id} number={index} />;
       })}
     </section>
   );
@@ -41,21 +28,6 @@ const BookList = () => {
 
 
 
-
-// --> approach 2 to use props by destructuring the prop object
-const Book = ({ img, author, title, getBook, id}) => {
-  const getSingleBook = () => {
-    getBook(id)
-  }
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <button onClick={()=>getBook(id)}>display title</button>
-      <h4>{author}</h4>
-    </article>
-  );
-};
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
